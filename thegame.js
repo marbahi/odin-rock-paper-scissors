5d5b10d7313e742f3ef251a9274e4591087cea03
marbahi <ammar.skyagt@gmail.com>
2025-07-28 22:57:58 +0700

    Write Logic
    Write all logic function in js file to make stucture of code programs

--- thegame.js
+++ thegame.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// get computer choice funtion
+// initialize variable choice
+// using math.random function to result 1-3 in choice var
+// if 1 return rock string
+// if 2 return paper string
+// if 3 return scissors string
+
+// get human choice function
+// initialize variable named choice
+// using prompt to get value
+// make new variable, valued lowercase variable choice
+
+// declare humanScore globally give value 0
+// declare computerScore globally give value 0
+
+// create function playRound
+// use parameter from computerChoice and humanChoice
+// if humanChoice rock, computerChoice paper, return "You Lose!, Paper beats Rock"
+// if humanChoice rock, computerChoice scissors, return "You Win!, Rock beats Scissors"
+// if humanChoice Paper, computerChoice rock, return "You Win!, Paper beats Rock"
+// if humanChoice Paper, computerChoice scissors, return "You Lose!, Scissors beats Paper"
+// if humanChoice Scissors, computerChoice rock, return "You Lose!, Rock beats Scissors"
+// if humanChoice Scissors, computerChoice paper, return "You Win!, Scissors beats Paper"
+// if human win humanScore +1
+// if computer win computerScore +1
+
+// create playGame function
+// using loops init gameCount value 0 with condition gameCount < 5, and +1 every iteration
